3cb1bdc6934cc3a6c4721fa5c02b1c275f6803b3
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-07-18 18:39:37 +0100

    Create a Web App Manifest

--- ALX/alx-pwa-0x01/alx-movie-app/pages/_app.tsx
+++ ALX/alx-pwa-0x01/alx-movie-app/pages/_app.tsx
@@ -1,11 +1,29 @@
-import Layout from "@/components/layout/Layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+    import type { AppProps } from 'next/app';
+    import Head from 'next/head';
+    import { useEffect } from 'react';
+
+    export default function MyApp({ Component, pageProps }: AppProps) {
+      useEffect(() => {
+        if ('serviceWorker' in navigator) {
+          window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js').then(registration => {
+              console.log('Service Worker registered:', registration.scope);
+            }).catch(error => {
+              console.error('Service Worker registration failed:', error);
+            });
+          });
+        }
+      }, []);
+
+      return (
+        <>
+          <Head>
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#FFFFFF" />
+          </Head>
+          <Component {...pageProps} />
+        </>
+      );
+    }
+   
